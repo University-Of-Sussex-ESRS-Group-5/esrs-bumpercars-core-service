2e4ce1eba561488e1829976505b6b4be17daf097
pbson <phambason639@gmail.com>
2024-05-13 18:00:22 +0100

    fix: update regex

--- src/modules/users/dtos/register.dto.ts
+++ src/modules/users/dtos/register.dto.ts
@@ -13,7 +13,7 @@ export class RegisterDTO {
 
   @ApiProperty({ required: true, type: 'string' })
   @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&=])[A-Za-z\d@$!%*?&=]{8,}$/,
     {
       message:
         'Password must contain at least 8 characters, one uppercase, one lowercase, one number, and one special character',
